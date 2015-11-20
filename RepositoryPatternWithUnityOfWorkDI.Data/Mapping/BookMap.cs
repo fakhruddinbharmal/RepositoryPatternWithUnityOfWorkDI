36543f38dc7ef976441d6302683284b7422a0eca
Fakhruddin Bharmal <bharmalfakhruddin@gmail.com>
2015-11-20 19:03:53 +0530

    First Commite

---
+++ RepositoryPatternWithUnityOfWorkDI.Data/Mapping/BookMap.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using RepositoryPatternWithUnitOfWorkDI.Core.Data;
+
+namespace RepositoryPatternWithUnityOfWorkDI.Data.Mapping;
+{
+    public class BookMap : EntityTypeConfiguration<Book>
+    {
+        public BookMap()
+        {
+            HasKey(t => t.ID);
+            Property(t => t.ID).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(t => t.Title).IsRequired();
+            Property(t => t.Author).IsRequired();
+            Property(t => t.ISBN).IsRequired();
+            Property(t => t.Published).IsRequired();
+            ToTable("Books");
+        }
+    }
+}
